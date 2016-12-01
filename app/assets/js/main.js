331ff7915a5a49571b8e7467693b5c10b25517cd
bramulous <bramvalstar@gmail.com>
2016-12-01 15:39:00 +0100

    Made droppoints, can snap couch to cursor

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,11 +1,22 @@
 $(document).ready(function () {
 
+    //variabele voor cursor en camera
+    var cursor = document.getElementById("cursor");
+    var camera = document.getElementById("camera");
+
     // behang muren
     var walltexture = $('.walltexture');
 
     // bank onderdelen
     var couchtexture = $('.couchtexture');
 
+    // variabelen voor de dropvlaktes
+    var drop1 = document.getElementById("dropvlak1");
+    var drop2 = document.getElementById("dropvlak2");
+
+    // variabelen van hele banken
+    var bank = document.getElementById("couch");
+    var bank2 = document.getElementById("couch3");
 
     // texture muren vakjes
     var wp1 = document.getElementById('wp1');
@@ -31,6 +42,31 @@ $(document).ready(function () {
     function addTexture(id, texture, mat1 ) {
         id.addEventListener('mouseenter', function () {
             mat1.attr('material', 'src :' + texture + '');
+        });
+    }
+
+    //aanmaken verschillende banken + functie
+    takeBank(bank);
+    takeBank(bank2);
+
+    //functie voor oppakken bank
+    function takeBank(id) {
+        id.addEventListener('mouseenter', function () {
+            //hieronder staat code waarbij in de cursor nu een box komt, dit moet de bank zelf worden
+            $(cursor).append("<a-entity id='bankRepresentation' position='0 -0.2 -0.03' geometry='depth:0.2;height:0.2;width:0.2'></a-entity>");
+            $(id).remove();
+        });
+    }
+
+    //aanmaken droppen bank + functie
+    dropBank(drop1);
+    dropBank(drop2);
+
+    //functie voor droppen bank
+    function dropBank(id) {
+        id.addEventListener('mouseenter', function () {
+            $(bankRepresentation).remove();
+            //hier moet nog code komen om bank weer neer te zetten
         });
     }
 
